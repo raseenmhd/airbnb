539bb0018cf10b4833f0ca026a177dde7fff33e9
raseenmhd <126247140+raseenmhd@users.noreply.github.com>
2024-02-22 15:59:34 +0530

    date picker and profile modal is completed

--- airbnb/js/scripts.js
+++ airbnb/js/scripts.js
@@ -1,26 +1,43 @@
 $(document).ready(function() { 
  
     $(function() { 
-        $("#my_date_picker1").
+        $("#date_picker1").
         datepicker({}); 
     }); 
 
     $(function() { 
-        $("#my_date_picker2").
+        $("#date_picker2").
         datepicker({}); 
     }); 
 
-    $('#my_date_picker1').change(function() { 
+    $('#date_picker1').change(function() { 
         startDate = $(this).
         datepicker('getDate'); 
-        $("#my_date_picker2").
+        $("#date_picker2").
         datepicker("option", "minDate", startDate); 
     }) 
 
-    $('#my_date_picker2').change(function() { 
+    $('#date_picker2').change(function() { 
         endDate = $(this).
         datepicker('getDate'); 
-        $("#my_date_picker1").
+        $("#date_picker1").
         datepicker("option", "maxDate", endDate); 
     }) 
+
+    $(function(){
+        document.querySelector('.profile').addEventListener('click', function(event){
+            event.stopPropagation();
+            document.querySelector(".profile_popup").style.display = (document.querySelector(".profile_popup")
+            .style.display === "block") ? "none" : "block";
+        })
+
+        document.querySelector('body').addEventListener('click', function(){
+            document.querySelector(".profile_popup").style.display = "none";
+        })  
+    })
 }) 
+
+
+
+
+
